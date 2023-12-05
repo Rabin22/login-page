0751cd36782b8277eb76bae3da7eecdd314fb4cb
DarkWolf-12 <dead.lovesagar@gmail.com>
2023-12-05 21:28:18 +0800

    changes

---
+++ login.js
@@ -0,0 +1,10 @@
+
+ 
+ 
+ function signup() { // for the sign up page to link with sign in page
+      
+     let websiteUrl = 'http://127.0.0.1:5500/login-page/sign_up.html ';
+     
+     // Open the website in a new tab or window
+     window.open(websiteUrl, '_blank');
+ } 
